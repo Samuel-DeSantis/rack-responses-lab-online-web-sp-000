e661b9c08992d763e70b6005395d55d3a71d7bde
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-03-15 03:09:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,14 @@
-class Time
-  attr_accessor :year, :month, :day, :hour, :minute
+class Application
 
-  def initialize(year, month, day, hour, minute)
-    @year = year
-    @month = month
-    @day = day
-    @hour = hour
-    @minute = minute
-  end
-
-  if @hour < 12
-    resp.write "Good Morning"
-  else
-    resp.write "Good Afternoon"
+  def call(env)
+    resp = Rack::Response.new
+    now = Time.now
+    if now.hour >= 12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
+    resp.finish
   end
 
 end
